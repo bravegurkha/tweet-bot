25eecf37d5340897dac01325dd9a6f25eeec928e
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-05 12:12:54 +0545

    Initial COmmit

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import tweepy,datetime,requests,json
 
 def tweet():
-    consumer_key = "fqP4meeNFELTr1CdXFoVt1Bdm"
-    consumer_secret = "PngIPv6xUvoWJlXNVVnTjOEpfVw3NvAPcHVHAypvtVqCu8zoaG"
-    access_token = "4644443353-hlM3yZbWt4c41quOMS5ylcJlSE3uu4PClvT6TO8"
-    access_token_secret = "ZALvEypcJm6i0c2tJYjAfhLIsvHxDYBXmfKRiu3n8Mb4x"
+    consumer_key = "<consumer_key>"
+    consumer_secret = "<consumer_secret>"
+    access_token = "<access_token>"
+    access_token_secret = "<access_token_secret>"
     quote_api_url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous" #you can use movies instead of famous. See in description.
 
     quoteData  = requests.get(quote_api_url,headers={
-        "X-Mashape-Key": "CJUssgbp0omsh4xqAaRXKVotTphKp1MkJ2SjsnArnsUa6jI4kN",
+        "X-Mashape-Key": "<X-Mashape-Key>",
         "Content-Type": "application/x-www-form-urlencoded",
         "Accept": "application/json"
       }).json()
